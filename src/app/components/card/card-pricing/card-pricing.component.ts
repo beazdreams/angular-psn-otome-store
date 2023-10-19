217906b4eabcdb6d57e48f4141e8f29ac63299fb
Beatriz <beapnhr102@gmail.com>
2023-10-19 20:52:32 -0300

    initial: project upload

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent implements OnInit {
+	@Input()
+	gameName:string = ""
+	@Input()
+	gameType:string ="Digital PS4"
+	@Input()
+	gamePrice:string = "R$ 399,90"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
